e9689dde181e45e283fc83ccf2a640b98e02c7c4
PatricioOsorio <patriciomiguel_12@hotmail.com>
2022-04-09 20:05:11 -0500

    Correciones

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,21 @@
 function generarColor() {
-    const caracteres = '0123456789ABCDEF';
-    const cadena = [];
-    for (let i = 0; i < 6; i++) {
-        let random = Math.floor(Math.random() * 16);
-        cadena.push(caracteres[random]);
-    }
-    return cadena.join('');
-};
+  const caracteres = '0123456789ABCDEF';
+  const cadena = [];
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * 16);
+    cadena.push(caracteres[random]);
+  }
+  return cadena.join('');
+}
 
 console.log(generarColor());
 
 const $hexadecimal = document.getElementById('hexadecimal');
 const $colorBody = document.getElementById('body');
 
-
 document.addEventListener('click', (e) => {
-    if (e.target.matches('#btn-cambiar')) {
-        $colorBody.style.backgroundColor = '#' + generarColor();
-        $hexadecimal.textContent = $colorBody.style.backgroundColor;
-    }
+  if (e.target.matches('.btn__cambiar')) {
+    $colorBody.style.backgroundColor = '#' + generarColor();
+    $hexadecimal.textContent = $colorBody.style.backgroundColor;
+  }
 });
-
-
